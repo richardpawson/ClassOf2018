f8ad631a1b2426823e1cbf69e22976cc7f011e56
Richard Pawson <rpawson@nakedobjects.net>
2017-03-19 20:32:38 +0000

    Added NakedObjectsTemplate

---
+++ NakedObjectsTemplate/Template.DataBase/ExampleDbContext.cs
@@ -0,0 +1,17 @@
+﻿
+using System.Data.Entity;
+using Template.Model;
+
+namespace Template.DataBase
+{
+    public class ExampleDbContext : DbContext
+    {
+        public ExampleDbContext(string dbName) : base(dbName)
+        {
+            Database.SetInitializer(new ExampleDbInitializer());
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+
+}
